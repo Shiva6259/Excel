23ff1b4da268be717592bed2a9a986a07005f4fd
shiva <sk735816@gmail.com>
2025-09-23 22:24:43 +0530

    third

--- src/test/java/DayOne.java
+++ src/test/java/DayOne.java
@@ -13,7 +13,7 @@ public class DayOne {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.google.com");
-		
+		System.out.println(driver.getTitle());
 	}
 
 }
