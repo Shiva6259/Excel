0b943e8f8b319f56eccde84af37168a92ccd6a2e
shiva <sk735816@gmail.com>
2025-09-24 22:23:53 +0530

    Broken Links

---
+++ src/test/java/BrokenLinks.java
@@ -0,0 +1,38 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinks {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\USER\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://www.deadlinkcity.com/");
+		List<WebElement> links=driver.findElements(By.tagName("a"));
+		for(WebElement link:links) {
+			String hrefvalue=link.getAttribute("href");
+			if(hrefvalue==null || hrefvalue.isEmpty())
+				System.out.println("Href is null or empty so cannot validate ");
+			try {
+				URL url=new URL(hrefvalue);
+				HttpURLConnection con=(HttpURLConnection) url.openConnection();
+				con.connect();
+				if(con.getResponseCode()>=400) {
+					System.out.println(hrefvalue+" Is broken url");
+				}else {
+					System.out.println(hrefvalue+" Is valid url");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
