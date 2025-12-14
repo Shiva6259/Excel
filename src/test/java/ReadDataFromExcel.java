bd93019091c7952f91d7603a877fa6234264f9da
shiva <sk735816@gmail.com>
2025-12-14 19:26:45 +0530

    ReadDataFromExcel

--- src/test/java/ReadDataFromExcel.java
+++ src/test/java/ReadDataFromExcel.java
@@ -1,14 +1,18 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ReadDataFromExcel {
-
-	public static void main(String[] args) throws IOException {
+	public static ArrayList getData(String testCaseName) throws IOException {
+		ArrayList<String> a=new ArrayList<String>();
 		FileInputStream fis=new FileInputStream("C:\\Users\\USER\\Documents\\Datadriven.xlsx");
 		XSSFWorkbook workbook=new XSSFWorkbook(fis);
 		int sheets=workbook.getNumberOfSheets();
@@ -27,15 +31,29 @@ public class ReadDataFromExcel {
 				}
 				while(rows.hasNext()) {
 					Row r=rows.next();
-					if(r.getCell(column).getStringCellValue().equalsIgnoreCase("purchase")) {
+					if(r.getCell(column).getStringCellValue().equalsIgnoreCase(testCaseName)) {
 						Iterator<Cell> cv=r.cellIterator();
 						while(cv.hasNext()) {
-							System.out.println(cv.next());
+							Cell c=cv.next();
+							if(c.getCellType()==CellType.STRING) {
+								a.add(c.getStringCellValue());								
+							}
+							else {
+								a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+								//a.add(cv.next().getNumericCellValue());
+							}
 						}
 					}
 				}
 			}
 		}
+		return a;
 	}
-
+	public static void main(String[] args) throws IOException {
+		ArrayList result=getData("add profile");
+		System.out.println(result.get(0));
+		System.out.println(result.get(1));
+		System.out.println(result.get(2));
+		System.out.println(result.get(3));
+	}
 }
